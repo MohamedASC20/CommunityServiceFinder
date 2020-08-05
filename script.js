50ad59c651abe1d00c233b6b8244b6beb05fc28d
MohamedASC2020 <mdrame1109@gmail.com>
2020-08-05 16:49:56 -0400

    updatedFirst Commit

--- script.js
+++ script.js
@@ -8,20 +8,8 @@ function initMap(){
     var map = new 
     google.maps.Map(document.getElementById('map'),options);
 /*
-    var marker = new google.maps.Marker({
-         position:{lat:40.812894,lng:-73.939472},
-         map:map,
-    });
-    var infoWindow = new google.maps.InfoWindow({
-     content: '<h1>Community Center</h1>'
-    })
-    marker.addListener('click',function(){
-        infoWindow.open(map, marker)
-    }
-    
-    
-    )*/
-    addMarker({lat:40.812894,lng:-73.939472})
+  
+     addMarker({lat:40.812894,lng:-73.939472})
     addMarker({lat:40.812894,lng:-73.939472})
     addMarker({lat:40.612882,lng:-73.938482})
    
@@ -31,6 +19,18 @@ function initMap(){
          map:map,
     });
     }
-
- }
+    
+    )*/
+     var marker = new google.maps.Marker({
+         position:{lat:40.812894,lng:-73.939472},
+         map:map,
+    });
+    
+  var infoWindow = new google.maps.InfoWindow({
+     content: 'Community Center'
+    })
+    marker.addListener('click',function(){
+        infoWindow.open(map, marker)
+    })
+}
 
